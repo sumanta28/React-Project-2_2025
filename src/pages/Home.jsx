72a84248cfb0e9961add8b4d211e8db9663613f5
Sumanta <sumanta2k16@gmail.com>
2025-07-15 02:58:00 +0530

    Fix case sensitivity for PaginationCard.jsx for Vercel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { getPokemonList } from "../api/pokemonapi";
 import PokemonCard from "../components/PokemonCard";
-import PaginationCard from "../components/PaginationCard";
+import PaginationCard from "../components/x";
 
 
 import {
